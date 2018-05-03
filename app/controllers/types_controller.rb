9f5a06c920d10c77ca738d720b743df560b97543
William Dale <williambdale@gmail.com>
2018-05-03 07:14:14 -0400

    Fix body type for types edit view

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,2 +1,37 @@
 class TypesController < ApplicationController
-end 
+
+  def index
+    @types = Type.all
+  end
+
+  def show
+    @type = Type.find(params[:id])
+  end
+
+  def new
+    @type = Type.new
+  end
+
+  def create
+    @type = Type.create(type_params)
+    redirect_to @type
+  end
+
+  def edit
+    @type = Type.find(params[:id])
+  end
+
+  def update
+    @type = Type.find(params[:id])
+    @type.update(type_params)
+    redirect_to @type
+  end
+
+
+  private
+
+  def type_params
+    params.require(:type).permit(:name, :body)
+  end
+
+end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,4 +1,9 @@
 class Type < ApplicationRecord
   has_many :beers
   has_many :flights, through: :beers
+
+  def type_list
+      %w(light mild medium full)
+  end
+
 end
